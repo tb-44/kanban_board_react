54f2669f65f9ffaf7cbe2bec0877cdb1ed393756
Trent Bennett <rehab.rtb@gmail.com>
2020-05-31 14:59:37 -0600

    adding main

---
+++ src/containers/Main.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import { getStatusRequest } from "../actions/auth";
+import Login from "../containers/Login";
+
+class Main extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    this.props.getStatusRequest().then(() => {
+      if (this.props.status === "SUCCESS") {
+        this.props.history.push("/mypage");
+      } else {
+        console.log("you need to login");
+      }
+    });
+  }
+
+  render() {
+    return <Login />;
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    status: state.auth.login.status,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getStatusRequest: () => {
+      return dispatch(getStatusRequest());
+    },
+  };
+};
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
