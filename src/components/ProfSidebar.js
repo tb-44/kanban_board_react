27c627e5e2d4feaf14153396728e53feaa706d2e
Trent Bennett <rehab.rtb@gmail.com>
2020-06-01 13:22:11 -0600

    implement more of sidebar and add searchbar

---
+++ src/components/ProfSidebar.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+import { Link, withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import { Menu, Icon } from "antd";
+
+class ProfSidebar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      classID: "",
+    };
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.getClassInfo.classID !== this.state.classID) {
+      this.setState({ classID: this.props.getClassInfo.classID });
+    }
+  }
+
+  render() {
+    return (
+      <Menu
+        className="sideMenu"
+        mode="inline"
+        defaultSelectedKeys={["4"]}
+        style={{ borderRight: 0, background: "none" }}
+      >
+        <Menu.Item key="1">
+          <Link to="/mypage">
+            <Icon type="home" />
+            <strong>My classroom</strong>
+          </Link>
+        </Menu.Item>
+        <Menu.Item key="2">
+          <Link to={`/classroom/${this.state.classID}/projectList`}>
+            <Icon type="bars" />
+            <strong>Project List</strong>
+          </Link>
+        </Menu.Item>
+        <Menu.Item key="3">
+          <Link to={`/classroom/${this.state.classID}/approve`}>
+            <Icon type="usergroup-add" />
+            <strong>Project Approval</strong>
+          </Link>
+        </Menu.Item>
+        <Menu.Item key="4">
+          <Link to={`/classroom/${this.state.classID}/notice`}>
+            <Icon type="notification" />
+            <strong>Notice</strong>
+          </Link>
+        </Menu.Item>
+      </Menu>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    getClassInfo: state.classroom.getClassInfo.info,
+  };
+};
+
+export default withRouter(connect(mapStateToProps)(ProfSidebar));
---
+++ src/components/Searchbar.js
@@ -0,0 +1,94 @@
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import { getSearchProjectRequest } from "../actions/search";
+import { Layout, Input, Icon, message } from "antd";
+const { Header } = Layout;
+
+class Searchbar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      inputdata: "",
+    };
+
+    this.emitEmpty = this.emitEmpty.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.onPressEnter = this.onPressEnter.bind(this);
+    this.getProject = this.getProject.bind(this);
+  }
+
+  emitEmpty() {
+    this.setState({ inputdata: "" });
+  }
+
+  onChange(e) {
+    let nextState = {};
+    nextState[e.target.id] = e.target.value;
+    this.setState(nextState);
+  }
+
+  onPressEnter() {
+    if (this.state.inputdata !== "") this.getProject();
+  }
+
+  getProject() {
+    this.props.getSearchProjectRequest(this.state.inputdata).then(() => {
+      if (this.props.status === "SUCCESS") {
+        message.success("Search complete.");
+        this.props.history.push("/mypage/search");
+      } else message.error("There was an error in search.");
+    });
+  }
+
+  render() {
+    const { inputdata } = this.state;
+    const suffix = inputdata ? (
+      <Icon type="close-circle" onClick={this.emitEmpty} />
+    ) : null;
+
+    return (
+      <Header
+        className="header"
+        style={{
+          width: "100%",
+          padding: "0 20px",
+          background: "rgb(255,255,255)",
+          borderBottom: "1px solid #ddd",
+        }}
+      >
+        <Input
+          id="inputdata"
+          className="searchInput"
+          placeholder="Search by classname, project name, student name..."
+          prefix={<Icon type="search" style={{ color: "rgba(0,0,0,.25)" }} />}
+          suffix={suffix}
+          value={inputdata}
+          onChange={this.onChange}
+          onPressEnter={this.onPressEnter}
+          size="large"
+        />
+      </Header>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    status: state.search.status,
+    project: state.search.project,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getSearchProjectRequest: (searchData) => {
+      return dispatch(getSearchProjectRequest(searchData));
+    },
+  };
+};
+
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(Searchbar)
+);
